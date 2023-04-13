8d1d2b784c984048f3f3ad64670eace8e0d23e0f
Glebzzzik <94132712+Glebzzzik@users.noreply.github.com>
2023-04-13 08:03:37 +0300

    Initial Commit

---
+++ ghjk/ghjkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghjkApp.swift
+//  ghjk
+//
+//  Created by Глеб Голощапов on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ghjkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
